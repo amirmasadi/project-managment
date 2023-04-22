b4a2c90e8dbda2b7b4913303a61c9f56735e20ce
amir <a.m.asadi2001@gmail.com>
2023-04-22 12:49:42 +0330

    change default error pages to custom ones

---
+++ src/main/java/com/amirasadi/pma/errors/errorController.java
@@ -0,0 +1,31 @@
+package com.amirasadi.pma.errors;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class errorController implements ErrorController {
+  @GetMapping("/error")
+  public String handleError(HttpServletRequest request){
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    if(status != null){
+      Integer statusCode = Integer.valueOf(status.toString());
+
+      if(statusCode == HttpStatus.NOT_FOUND.value()){
+        return "errors/404";
+      }
+      if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()){
+        return "errors/500";
+      }
+      if(statusCode == HttpStatus.FORBIDDEN.value()){
+        return "errors/403";
+      }
+    }
+    return "errors/someThingWentWrong";
+  }
+}
